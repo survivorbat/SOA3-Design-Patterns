1c411a72228c9a5173142dbda0b755015241e4bc
Gerben Droogers <gerben@droogers.eu>
2019-02-17 21:20:04 +0100

    Make tests working

--- src/tests/DomainBundle/Unit/Entity/UserTest.php
+++ src/tests/DomainBundle/Unit/Entity/UserTest.php
@@ -6,8 +6,40 @@ use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
-    public function emptyTest(): void
+    public function testIdSet(): void
     {
-        $this->assertTrue(true);
+        $user = new User();
+        $user->setId('0');
+        $this->assertEquals("0", $user->getId());
+    }
+    public function testUserNameSet(): void
+    {
+        $user = new User();
+        $user->setUserName('0');
+        $this->assertEquals("0", $user->getUserName());
+    }
+    public function testFirstNameSet(): void
+    {
+        $user = new User();
+        $user->setFirstName('0');
+        $this->assertEquals("0", $user->getFirstName());
+    }
+    public function testLastNameSet(): void
+    {
+        $user = new User();
+        $user->setLastName('0');
+        $this->assertEquals("0", $user->getLastName());
+    }
+    public function testEmailSet(): void
+    {
+        $user = new User();
+        $user->setEmailAddress('0');
+        $this->assertEquals("0", $user->getEmailAddress());
+    }
+    public function testPhoneSet(): void
+    {
+        $user = new User();
+        $user->setPhoneNumber('0');
+        $this->assertEquals("0", $user->getPhoneNumber());
     }
 }
